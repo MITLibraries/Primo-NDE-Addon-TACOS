a9882631adf5464ff29866e62409868d2c73019b
Adam Shire <adamshire123@gmail.com>
2026-02-10 17:48:38 -0500

    get unit tests running

--- src/app/tacos-module/tacos.component.spec.ts
+++ src/app/tacos-module/tacos.component.spec.ts
@@ -1,23 +1,49 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { TacosComponent } from './tacos.component';
+import { of } from 'rxjs';
+import { provideMockStore } from '@ngrx/store/testing';
+import { TacosComponent, selectSearchTerm } from './tacos.component';
+import { TacosService } from './tacos.service';
 
 describe('TacosComponent', () => {
-  let component: TacosComponent;
   let fixture: ComponentFixture<TacosComponent>;
+  let component: TacosComponent;
+  let tacosServiceMock: jasmine.SpyObj<TacosService>;
+  const MOCK_TACOS_RESPONSE = {
+    data: {
+      logSearchEvent: {
+        detectors: {
+          suggestedResources: [
+            { url: 'https://taco1.com', title: 'Taco One' },
+            { url: 'https://taco2.com', title: 'Taco Two' }
+          ]
+        }
+      }
+    }
+  };
 
   beforeEach(async () => {
+    tacosServiceMock = jasmine.createSpyObj('TacosService', ['getTacosResponse']);
+    tacosServiceMock.getTacosResponse.and.returnValue(of(MOCK_TACOS_RESPONSE));
+
     await TestBed.configureTestingModule({
-      imports: [TacosComponent]
-    })
-    .compileComponents();
+      imports: [TacosComponent],
+      providers: [
+        provideMockStore({
+          selectors: [
+            { selector: selectSearchTerm, value: 'foo' }
+          ]
+        }),
+        { provide: TacosService, useValue: tacosServiceMock }
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TacosComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('calls the service when search term exists', () => {
+    fixture.detectChanges();
+
+    expect(tacosServiceMock.getTacosResponse).toHaveBeenCalledWith('foo');
   });
 });
--- src/app/tacos-module/tacos.service.spec.ts
+++ src/app/tacos-module/tacos.service.spec.ts
@@ -1,13 +1,19 @@
 import { TestBed } from '@angular/core/testing';
-
+import { provideHttpClient, } from '@angular/common/http';
+import { provideHttpClientTesting, HttpTestingController } from '@angular/common/http/testing';
 import { TacosService } from './tacos.service';
 
 describe('TacosService', () => {
   let service: TacosService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()]
+    });
     service = TestBed.inject(TacosService);
+    const httpTesting = TestBed.inject(HttpTestingController)
   });
 
   it('should be created', () => {
